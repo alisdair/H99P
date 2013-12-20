cefca726f4702ee58886bed659580740f6212751
Alisdair McDiarmid <alisdair@mcdiarmid.org>
2013-12-20 22:34:17 +0000

    Solution for problem 2: butLast.

--- Lists.hs
+++ Lists.hs
@@ -1,9 +1,13 @@
 module Lists
   (
   last
+  ,butLast
   ) where
 
 import Prelude hiding (last)
 
 last :: [a] -> a
 last = head . reverse
+
+butLast :: [a] -> a
+butLast = head . tail . reverse
--- Spec.hs
+++ Spec.hs
@@ -11,3 +11,10 @@ main = hspec $ do
 
     it "throws an exception if used with an empty list" $ do
       evaluate (Lists.last []) `shouldThrow` anyException
+
+  describe "Lists.butLast" $ do
+    it "returns the second-last element of a list" $ do
+      Lists.butLast [1, 2, 3] `shouldBe` (2 :: Int)
+
+    it "throws an exception if used with a unit list" $ do
+      evaluate (Lists.butLast [1]) `shouldThrow` anyException
